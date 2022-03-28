ef5add0d6dc68f7c436d3ebf2c2b42c3b2a3c25a
test <test>
2022-03-28 18:54:07 +0300

    Initial commit

---
+++ task.js
@@ -0,0 +1,13 @@
+function solveEquation(a, b, c) {
+  let res=[]
+	let D = Math.pow(b,2) - 4 * a * c
+	if( D == 0){
+    res.push(-b/(2*a))
+  }
+	else if(D>0) {
+		res.push((-b + Math.sqrt(D)) / (2 * a))
+    res.push((-b - Math.sqrt(D)) / (2 * a))
+	}
+	return res
+}
+
