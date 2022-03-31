3138ad503fa1273702a351b1f570e46b3fbbb05a
test <test>
2022-03-31 12:51:06 +0300

    format fix

--- task.js
+++ task.js
@@ -1,10 +1,10 @@
 function solveEquation(a, b, c) {
-  let res=[]
-	let D = Math.pow(b,2) - 4 * a * c
+  let res = []
+	let D = Math.pow(b, 2) - 4 * a * c
 	if( D == 0){
-    res.push(-b/(2*a))
+    res.push( -b / ( 2 * a ))
   }
-	else if(D>0) {
+	else if(D > 0) {
 		res.push((-b + Math.sqrt(D)) / (2 * a))
     res.push((-b - Math.sqrt(D)) / (2 * a))
 	}
